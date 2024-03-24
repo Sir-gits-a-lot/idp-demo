9bff2f6d0d95f8f33374dea2d61b88bc283853fc
Sir-gits-a-lot <85702060+Sir-gits-a-lot@users.noreply.github.com>
2024-03-24 23:31:51 +0530

    Update create-cluster.sh

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -1,8 +1,9 @@
 NAME=$1
 PROVIDER=$2
 CLUSTER=$3
-NODE_SIZE=$4
-MIN_NODE_COUNT=$5
+NODE_SIZE=$5
+VERSION=$4
+MIN_NODE_COUNT=$6
 
 FILE_PATH=infra/${NAME}-cluster.yaml
 
@@ -12,4 +13,5 @@ yq --inplace ".spec.id = \"${NAME}\"" $FILE_PATH
 yq --inplace ".spec.compositionSelector.matchLabels.provider = \"${PROVIDER}\"" $FILE_PATH
 yq --inplace ".spec.compositionSelector.matchLabels.cluster = \"${CLUSTER}\"" $FILE_PATH
 yq --inplace ".spec.parameters.nodeSize = \"${NODE_SIZE}\"" $FILE_PATH
+yq --inplace ".spec.parameters.version = \"${VERSION}\"" $FILE_PATH
 yq --inplace ".spec.parameters.minNodeCount = ${MIN_NODE_COUNT}" $FILE_PATH
