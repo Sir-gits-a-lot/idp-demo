5502edce39aaeb4f9168834a60671e2af4c17121
Sir-gits-a-lot <85702060+Sir-gits-a-lot@users.noreply.github.com>
2024-03-25 19:59:09 +0530

    Update create-local-dev-cluster.sh

--- scripts/create-local-dev-cluster.sh
+++ scripts/create-local-dev-cluster.sh
@@ -2,5 +2,5 @@ NAME=$1
 
 FILE_PATH=local-dev/${NAME}-cluster.yaml
 
-cp crossplane/cluster-template.yaml $FILE_PATH
+cp k3d/cluster-template.yaml $FILE_PATH
 yq --inplace ".metadata.name = \"${NAME}\"" $FILE_PATH
